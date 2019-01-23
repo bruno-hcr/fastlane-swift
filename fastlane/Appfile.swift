d9a33efa7cfd8d9f6f18b1f67b3a90c33e95a48e
Bruno Ramos <bruno-hcr@hotmail.com>
2019-01-23 00:49:38 -0200

    Adding some example lanes

--- fastlane/Appfile.swift
+++ fastlane/Appfile.swift
@@ -1,5 +1,5 @@
-var appIdentifier: String { return "[[APP_IDENTIFIER]]" } // The bundle identifier of your app
-var appleID: String { return "[[APPLE_ID]]" } // Your Apple email address
+var appIdentifier: String { return "[[Cocoaheads]]" } // The bundle identifier of your app
+var appleID: String { return "[[bruno-hcr@hotmail.com]]" } // Your Apple email address
 
 
 
--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,8 +9,49 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
+	func testLane() {
+	desc("Run Build and Tests")
+		scan()
 	}
+    
+    func betaLane() {
+        desc("Publish a beta version on Hockey App")
+        match(type: "ad-hoc",
+              readonly: true)
+        
+        gym(scheme: "Cocoaheads",
+            clean: true,
+            outputDirectory: "beta",
+            outputName: "cocoaheads")
+        
+        hockey(apiToken: "123456",
+               ipa: "./beta/cocoaheads.ipa",
+               notes: "Agora é possível escrever suas lanes em Swift. 0/")
+    }
+    
+    func publishAppStoreLane(withOptions options:[String: String]?) {
+        desc("Publish app on App Store")
+        incrementBuildNumber(buildNumber: options?["build_number"])
+        incrementVersionNumber(versionNumber: options?["version_number"])
+        
+        testLane()
+        
+        match(type: "app-store",
+              readonly: true)
+        
+        gym(scheme: "Cocoaheads",
+            clean: true,
+            outputDirectory: "build",
+            outputName: "cocoaheads")
+        
+        deliver(username: appleID,
+                appIdentifier: appIdentifier,
+                ipa: "build/cocoaheads.ipa",
+                skipBinaryUpload: true,
+                skipScreenshots: true,
+                skipMetadata: false,
+                submitForReview: false,
+                automaticRelease: false,
+                phasedRelease: true)
+    }
 }
